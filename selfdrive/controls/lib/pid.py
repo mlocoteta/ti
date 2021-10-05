ed62a1ebf0f82443f1ee7f1d30d94aebbdd6aa59
Mathew Locoteta <m.locoteta@gmail.com>
2021-10-04 23:04:01 -0400

    attempt to add op_edit PID tuning only

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -1,5 +1,9 @@
 import numpy as np
 from common.numpy_fast import clip, interp
+from common.op_params import opParams
+from common.realtime import DT_CTRL
+from selfdrive.config import Conversions as CV
+
 
 def apply_deadzone(error, deadzone):
   if error > deadzone:
@@ -10,11 +14,13 @@ def apply_deadzone(error, deadzone):
     error = 0.
   return error
 
-class PIController():
-  def __init__(self, k_p, k_i, k_f=1., pos_limit=None, neg_limit=None, rate=100, sat_limit=0.8, convert=None):
+class LatPIDController():
+  def __init__(self, k_p, k_i, k_d, k_f=1., pos_limit=None, neg_limit=None, rate=100, sat_limit=0.8, convert=None):
     self._k_p = k_p  # proportional gain
     self._k_i = k_i  # integral gain
+    self._k_d = k_d  # derivative gain
     self.k_f = k_f  # feedforward gain
+    self.op_params = opParams()
 
     self.pos_limit = pos_limit
     self.neg_limit = neg_limit
@@ -29,11 +35,18 @@ class PIController():
 
   @property
   def k_p(self):
-    return interp(self.speed, self._k_p[0], self._k_p[1])
+    return self.op_params.get('lat_p')
+    # return interp(self.speed, self._k_p[0], self._k_p[1])
 
   @property
   def k_i(self):
-    return interp(self.speed, self._k_i[0], self._k_i[1])
+    return self.op_params.get('lat_i')
+    # return interp(self.speed, self._k_i[0], self._k_i[1])
+
+  @property
+  def k_d(self):
+    return self.op_params.get('lat_d')
+    # return interp(self.speed, self._k_d[0], self._k_d[1])
 
   def _check_saturation(self, control, check_saturation, error):
     saturated = (control < self.neg_limit) or (control > self.pos_limit)
@@ -54,6 +67,8 @@ class PIController():
     self.sat_count = 0.0
     self.saturated = False
     self.control = 0
+    self.errors = []
+    self.delayed_output = 0.
 
   def update(self, setpoint, measurement, speed=0.0, check_saturation=True, override=False, feedforward=0., deadzone=0., freeze_integrator=False):
     self.speed = speed
@@ -61,12 +76,18 @@ class PIController():
     error = float(apply_deadzone(setpoint - measurement, deadzone))
     self.p = error * self.k_p
     self.f = feedforward * self.k_f
+    print("update kf ", self.k_f)
+    print("update feedforward ", feedforward)
+    d = 0
+    if len(self.errors) >= 5:  # makes sure list is long enough
+      d = (error - self.errors[-5]) / 5  # get deriv in terms of 100hz (tune scale doesn't change)
+      d *= self.k_d
 
     if override:
       self.i -= self.i_unwind_rate * float(np.sign(self.i))
     else:
       i = self.i + error * self.k_i * self.i_rate
-      control = self.p + self.f + i
+      control = self.p + self.f + i + d
 
       if self.convert is not None:
         control = self.convert(control, speed=self.speed)
@@ -78,11 +99,118 @@ class PIController():
          not freeze_integrator:
         self.i = i
 
-    control = self.p + self.f + self.i
+    control = self.p + self.f + self.i + d
+    if self.convert is not None:
+      control = self.convert(control, speed=self.speed)
+###
+    alpha = 1. - DT_CTRL / (self.op_params.get('lat_rc') + DT_CTRL)
+    self.delayed_output = self.delayed_output * alpha + control * (1. - alpha)
+    control = float(self.delayed_output) + self.f
+###
+    self.saturated = self._check_saturation(control, check_saturation, error)
+
+    self.errors.append(float(error))
+    while len(self.errors) > 5:
+      self.errors.pop(0)
+
+    self.control = clip(control, self.neg_limit, self.pos_limit)
+    return self.control
+
+
+class LongPIDController:
+  def __init__(self, k_p, k_i, k_d, k_f=1., pos_limit=None, neg_limit=None, rate=100, sat_limit=0.8, convert=None):
+    self.op_params = opParams()
+    self._k_p = k_p  # proportional gain
+    self._k_i = k_i  # integral gain
+    self._k_d = k_d  # derivative gain
+    self.k_f = k_f  # feedforward gain
+
+    self.max_accel_d = 0.4 * CV.MPH_TO_MS
+
+    self.pos_limit = pos_limit
+    self.neg_limit = neg_limit
+
+    self.sat_count_rate = 1.0 / rate
+    self.i_unwind_rate = 0.3 / rate
+    self.rate = 1.0 / rate
+    self.sat_limit = sat_limit
+    self.convert = convert
+
+    self.reset()
+
+  @property
+  def k_p(self):
+    return interp(self.speed, self._k_p[0], self._k_p[1])
+
+  @property
+  def k_i(self):
+    return interp(self.speed, self._k_i[0], self._k_i[1])
+
+  @property
+  def k_d(self):
+    return interp(self.speed, self._k_d[0], self._k_d[1])
+
+  def _check_saturation(self, control, check_saturation, error):
+    saturated = (control < self.neg_limit) or (control > self.pos_limit)
+
+    if saturated and check_saturation and abs(error) > 0.1:
+      self.sat_count += self.sat_count_rate
+    else:
+      self.sat_count -= self.sat_count_rate
+
+    self.sat_count = clip(self.sat_count, 0.0, 1.0)
+
+    return self.sat_count > self.sat_limit
+
+  def reset(self):
+    self.p = 0.0
+    self.id = 0.0
+    self.f = 0.0
+    self.sat_count = 0.0
+    self.saturated = False
+    self.control = 0
+    self.last_setpoint = 0.0
+    self.last_error = 0.0
+
+  def update(self, setpoint, measurement, speed=0.0, check_saturation=True, override=False, feedforward=0., deadzone=0., freeze_integrator=False):
+    self.speed = speed
+    print("update kf before ", self.k_f)
+
+    error = float(apply_deadzone(setpoint - measurement, deadzone))
+    self.k_f = self.op_params.get('lat_f')/100000
+    self.p = error * self.k_p
+    self.f = feedforward * self.k_f
+    print("update kf after ", self.k_f)
+    if override:
+      self.id -= self.i_unwind_rate * float(np.sign(self.id))
+    else:
+      i = self.id + error * self.k_i * self.rate
+      control = self.p + self.f + i
+
+      if self.convert is not None:
+        control = self.convert(control, speed=self.speed)
+
+      # Update when changing i will move the control away from the limits
+      # or when i will move towards the sign of the error
+      if ((error >= 0 and (control <= self.pos_limit or i < 0.0)) or \
+          (error <= 0 and (control >= self.neg_limit or i > 0.0))) and \
+              not freeze_integrator:
+        self.id = i
+
+    # if self.op_params.get('enable_long_derivative'):
+    #   if abs(setpoint - self.last_setpoint) / self.rate < self.max_accel_d:  # if setpoint isn't changing much
+    #     d = self.k_d * (error - self.last_error)
+    #     if (self.id > 0 and self.id + d >= 0) or (self.id < 0 and self.id + d <= 0):  # if changing integral doesn't make it cross zero
+    #       self.id += d
+
+    control = self.p + self.f + self.id
     if self.convert is not None:
       control = self.convert(control, speed=self.speed)
 
     self.saturated = self._check_saturation(control, check_saturation, error)
+
+    self.last_setpoint = float(setpoint)
+    self.last_error = float(error)
 
     self.control = clip(control, self.neg_limit, self.pos_limit)
     return self.control
