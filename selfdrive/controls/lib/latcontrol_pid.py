bfd44c92fe7515505d5135702926c2016c69012c
Mathew Locoteta <m.locoteta@gmail.com>
2021-10-30 23:48:52 -0400

    Add PID Tuning and OP_EDIT.py

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -1,16 +1,30 @@
 import math
 
+<<<<<<< HEAD
 from selfdrive.controls.lib.pid import PIDController
+=======
+from selfdrive.controls.lib.pid import LatPIDController
+>>>>>>> bfa01b00... Add PID Tuning and OP_EDIT.py
 from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import log
+from common.op_params import opParams
 
 
 class LatControlPID():
   def __init__(self, CP):
+<<<<<<< HEAD
     self.pid = PIDController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
                              (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
                              (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
                              k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, sat_limit=CP.steerLimitTimer, derivative_period=0.1)
+=======
+    self.pid = LatPIDController((CP.lateralTuning.pid.kpBP, CP.lateralTuning.pid.kpV),
+                                (CP.lateralTuning.pid.kiBP, CP.lateralTuning.pid.kiV),
+                                (CP.lateralTuning.pid.kdBP, CP.lateralTuning.pid.kdV),
+                                k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, sat_limit=CP.steerLimitTimer, derivative_period=0.1)
+    self.new_kf_tuned = CP.lateralTuning.pid.newKfTuned
+    self.op_params = opParams()
+>>>>>>> bfa01b00... Add PID Tuning and OP_EDIT.py
 
   def reset(self):
     self.pid.reset()
@@ -23,6 +37,7 @@ class LatControlPID():
     angle_steers_des_no_offset = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo))
     angle_steers_des = angle_steers_des_no_offset + params.angleOffsetDeg
 
+    pid_log.angleError = angle_steers_des - CS.steeringAngleDeg
     if CS.vEgo < 0.3 or not active:
       output_steer = 0.0
       pid_log.active = False
@@ -34,13 +49,17 @@ class LatControlPID():
 
       # TODO: feedforward something based on lat_plan.rateSteers
       steer_feedforward = angle_steers_des_no_offset  # offset does not contribute to resistive torque
-      steer_feedforward *= CS.vEgo**2  # proportional to realigning tire momentum (~ lateral accel)
+      if self.new_kf_tuned:
+        _c1, _c2, _c3 = 0.35189607550172824, 7.506201251644202, 69.226826411091
+        steer_feedforward *= _c1 * CS.vEgo ** 2 + _c2 * CS.vEgo + _c3
+      else:
+        steer_feedforward *= CS.vEgo ** 2  # proportional to realigning tire momentum (~ lateral accel)
 
       deadzone = 0.0
 
       check_saturation = (CS.vEgo > 10) and not CS.steeringRateLimited and not CS.steeringPressed
       output_steer = self.pid.update(angle_steers_des, CS.steeringAngleDeg, check_saturation=check_saturation, override=CS.steeringPressed,
-                                     feedforward=steer_feedforward, speed=CS.vEgo, deadzone=deadzone)
+                                     feedforward=steer_feedforward * self.op_params.get('lat_f_multiplier'), speed=CS.vEgo, deadzone=deadzone)
       pid_log.active = True
       pid_log.p = self.pid.p
       pid_log.i = self.pid.i
