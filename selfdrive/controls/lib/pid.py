baed2dfdf3bab2b4dfc7f2a6e23c46ad5d53ab17
Mathew Locoteta <m.locoteta@gmail.com>
2021-11-13 14:45:09 -0500

    trying again?

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -94,6 +94,7 @@ class PIDController():
   def reset(self):
     self.p = 0.0
     self.i = 0.0
+    self.d = 0.0
     self.f = 0.0
     self.sat_count = 0.0
     self.saturated = False
@@ -113,15 +114,12 @@ class PIDController():
     if len(self.errors) >= self._d_period:  # makes sure we have enough history for period
       d = (error - self.errors[-self._d_period]) / self._d_period  # get deriv in terms of 100hz (tune scale doesn't change)
       d *= self.k_d
-
+    self.d = d
     if override:
       self.i -= self.i_unwind_rate * float(np.sign(self.i))
     else:
       i = self.i + error * self.k_i * self.i_rate
       control = self.p + self.f + i + d
-
-      if self.convert is not None:
-        control = self.convert(control, speed=self.speed)
 
       # Update when changing i will move the control away from the limits
       # or when i will move towards the sign of the error
@@ -131,13 +129,6 @@ class PIDController():
         self.i = i
 
     control = self.p + self.f + self.i + d
-    if self.convert is not None:
-      control = self.convert(control, speed=self.speed)
-
-#    alpha = 1. - DT_CTRL / (self.op_params.get('lat_rc') + DT_CTRL)
-#    self.delayed_output = self.delayed_output * alpha + control * (1. - alpha)
-#    control = float(self.delayed_output) + self.f
-#
     self.saturated = self._check_saturation(control, check_saturation, error)
 
     self.errors.append(float(error))
