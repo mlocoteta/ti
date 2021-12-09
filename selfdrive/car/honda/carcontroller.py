9afa14ca8136e21f54188d5bf35313813246ddb9
Comma Device <device@comma.ai>
2021-12-09 18:49:40 -0500

    Tweaks

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -154,17 +154,17 @@ class CarController():
     apply_steer = int(interp(actuators.steer * P.STEER_MAX, P.STEER_LOOKUP_BP, P.STEER_LOOKUP_V))
 
     if (CS.CP.carFingerprint in SERIAL_STEERING): # Dynamic torque boost if above threshold, smooth torque blend otherwise
-      if (apply_steer >= self.steer_torque_boost_min) or (apply_steer <= -self.steer_torque_boost_min):
-        apply_steer = apply_serial_steering_torque_mod(apply_steer, self.steer_torque_boost_min, self.apply_steer_warning_counter, self.apply_steer_cooldown_counter)
-      else:
-        apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
-        self.apply_steer_warning_counter = 0
-        self.apply_steer_cooldown_counter = 0
+#      if (apply_steer >= self.steer_torque_boost_min) or (apply_steer <= -self.steer_torque_boost_min):
+#        apply_steer = apply_serial_steering_torque_mod(apply_steer, self.steer_torque_boost_min, self.apply_steer_warning_counter, self.apply_steer_cooldown_counter)
+#      else:
+      apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, self.params)
+#        self.apply_steer_warning_counter = 0
+#        self.apply_steer_cooldown_counter = 0
       self.apply_steer_last = apply_steer
 
     # Add low steering torque cap for low speeds, manual turning, & manual interventions
-    if (CS.steer_torque_limited and (apply_steer > 60 or apply_steer < -60)):
-      apply_steer = 60 if apply_steer > 0 else -60
+#    if (CS.steer_torque_limited and (apply_steer > 60 or apply_steer < -60)):
+#      apply_steer = 60 if apply_steer > 0 else -60
 
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_steer = -apply_steer
--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -237,7 +237,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 13.66 # 13.37 is spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.29], [0.07]]      
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.26], [0.07]]      
       ret.lateralTuning.pid.kf = 0.000025
       tire_stiffness_factor = 0.8467
 
